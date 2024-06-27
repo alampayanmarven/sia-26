37cb44a97681a8fd1f41fc1f75d41d490afa5ed1
Tama si gabriel <alampayanmarven9@gmail.com>
2024-06-27 13:42:00 +0800

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+@extends('templates.base')
+
+@section('content')
+    <h1 class="text-xl">
+        About us
+    </h1>
+    <br>
+    <p>she was the most beautiful girl I had ever seen</p>
+    <p>she was the most beautiful girl I had ever seen</p>
+    <p>she was the most beautiful girl I had ever seen</p>
+@endsection
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('templates.base')
+
+@section('content')
+    <h1 class="text-xl">
+        Welcome!
+    </h1>
+<br>
+    <p>she was the most beautiful girl I had ever seen</p>
+    <p>she was the most beautiful girl I had ever seen</p>
+    <p>she was the most beautiful girl I had ever seen</p>
+@endsection
